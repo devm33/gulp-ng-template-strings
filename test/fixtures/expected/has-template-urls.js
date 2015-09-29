cef4ea8ce59b3f2c2e516f34ecc2e5db7473663e
Devraj Mehta <devm33@gmail.com>
2015-09-29 03:26:47 -0400

    added exepected case for replacing templateUrls

---
+++ test/fixtures/expected/has-template-urls.js
@@ -0,0 +1,19 @@
+console.module('test', [])
+
+    .directive('first', function() {
+        return {
+            restrict: 'E',
+            templateUrl: '<div><h1>First Template</h1></div>',
+            controler: function() {
+                console.log();
+            }
+        };
+    })
+
+    .directive('second', second);
+
+function second() {
+    return {
+        templateUrl: '<ul><li>Second</li><li>Template</li></ul>'
+    };
+}
